98dc0b362787ac5f5cd678cf6a2d68112452de2c
Max R <maxx.ratto@gmail.com>
2020-01-22 07:37:37 -0700

    New commit.

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-    res.send('hello wwld');
+app.use(function validateBearerToken(req, res, next) {
+    console.log('validate bearer token middleware')
+    debugger
+    // move to the next middleware
+    next()
 })
 
-app.listen(8000, () => {
-    console.log('Now listening on port 8000.')
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req, res) {
+    res.json(validTypes);
+};
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+    res.send('Hello pokemon');
+}
+
+app.get('/pokemon', handleGetPokemon);
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
 })
