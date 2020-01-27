1171effbc29a37e4b0f5f8044194810d21667d2e
Max R <maxx.ratto@gmail.com>
2020-01-27 16:33:34 -0700

    Preparing for deployment

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
-
-console.log(process.env.API_TOKEN)
+const cors = require('cors');
+const POKEDEX = require('./pokedex.json');
+const helmet = require('helmet');
 
 const app = express()
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
-    console.log('validate bearer token middleware')
-    debugger
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
     // move to the next middleware
     next()
 })
@@ -24,13 +32,37 @@ function handleGetTypes(req, res) {
 app.get('/types', handleGetTypes);
 
 function handleGetPokemon(req, res) {
-    res.send('Hello pokemon');
+    let response = POKEDEX.pokemon;
+
+    if (req.query.name) {
+        response = response.filter(pokemon =>
+            pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+        )
+    }
+
+    if (req.query.type) {
+        response = response.filter(pokemon =>
+            pokemon.type.includes(req.query.type)
+        )
+    }
+
+    res.json(response);
 }
 
 app.get('/pokemon', handleGetPokemon);
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
+    console.log('Server listening.')
 })
